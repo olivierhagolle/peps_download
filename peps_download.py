566c010c19ca270a92a235ec34c76c6a27ff1d54
floriandeboissieu <fdeboiss@gmail.com>
2020-10-30 01:04:36 +0100

    change output of peps_download

--- peps_download.py
+++ peps_download.py
@@ -419,7 +419,7 @@ def peps_download(write_dir, auth, collection='S2', product_type="", sensor_mode
     os.system(search_catalog)
     time.sleep(5)
 
-    prod, download_dict, storage_dict, size_dict = parse_catalog(search_json_file, orbit, collection, clouds, sat)
+    products, download_dict, storage_dict, size_dict = parse_catalog(search_json_file, orbit, collection, clouds, sat)
 
     # ====================
     # Download
